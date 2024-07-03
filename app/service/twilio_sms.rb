903d59a8c102d82fcfc3f4a9474d5b5bfd9a8f46
sanket1.tikhande <sanket1.tikhande@ril.com>
2024-07-03 13:48:06 +0530

    added new changes

--- app/service/twilio_sms.rb
+++ app/service/twilio_sms.rb
@@ -5,11 +5,7 @@ class TwilioSms
     attr_accessor :mobile_number, :message, :account_sid, :auth_token, :service
 
     def initialize(mobile_number, message)
-    	# @account_sid = 'AC348e9e4776a6a3a2fbaf6ba8f90b68fd' 
-   		# @auth_token = '929c7ce766182d618c44873fd56c290a'
-   		# @service = 'MG1130bb962ec8ab447d9754e832569db6'	
-   		# @message = message
-   		# @mobile_number = mobile_number
+    	
 	end 
 
 
